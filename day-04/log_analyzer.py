e6c64baf1e1dd9048cfd0f9271e238a789ba33b4
denishtandel2493-blip <denishtandel2493@gmail.com>
2025-12-28 20:01:50 +0530

    day-04_solution.py

--- day-04/log_analyzer.py
+++ day-04/log_analyzer.py
@@ -1,8 +1,29 @@
-import json
 
 def read_logs():
   with open("app.log","r") as file:
-  print(file.readlines())
+    return file.readlines()
 
-read_logs()
+def analyze(lines):
+  log_count = {
+              "INFO": 0,
+              "WARNING": 0,
+              "ERROR": 0
+              }
+  
+  for line in lines:
+      if "INFO" in line:
+          log_count["INFO"] += 1
+      elif "WARNING" in line:
+          log_count["WARNING"] += 1
+      elif "ERROR" in line:
+          log_count["ERROR"] += 1
+      else:
+        pass
+    
+  return log_count
+
+lines = read_logs()
+counts = analyze(lines)
+print("log counts are: ", counts)
+
 
